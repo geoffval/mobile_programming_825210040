2f2b26365308c7da20ad678fd755bf828cae963a
geoffval <geoffrey.825210040@stu.untar.ac.id>
2023-09-17 12:25:30 +0700

    Update to pertemuan3

--- pertemuan3/lib/main.dart
+++ pertemuan3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pertemuan3/login_screen.dart';
 import 'stopwatch.dart';
 
 void main() => runApp(StopwatchApp());
@@ -6,7 +7,7 @@ void main() => runApp(StopwatchApp());
 class StopwatchApp extends StatelessWidget{
   Widget build (BuildContext context){
     return MaterialApp(
-      home: StopWatch(),
+      home: LoginScreen(),
     );
   }
 }
--- pertemuan3/lib/stopwatch.dart
+++ pertemuan3/lib/stopwatch.dart
@@ -2,6 +2,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class StopWatch extends StatefulWidget {
+  final String name;
+  final String email;
+
+  const StopWatch({Key? key,required this.name,required this.email}) :super(key: key);
 
   @override
   State createState() => StopWatchState();
@@ -10,7 +14,11 @@ class StopWatch extends StatefulWidget {
 class StopWatchState extends State<StopWatch> {
   bool isTicking = true;
   int seconds = 0;
+  int milliseconds = 0;
   late Timer timer;
+  final laps = <int>[];
+  final itemHeight = 60.0;
+  final scrollController = ScrollController();
 
   /*
   @override
@@ -23,11 +31,17 @@ class StopWatchState extends State<StopWatch> {
 
   void _onTick(Timer time){
     setState(() {
-      ++seconds;
+      //++seconds;
+      milliseconds += 100;
     });
   }
 
-  String _secondsString() => seconds == 1 ? 'second' : 'seconds';
+  //String _secondsString() => seconds == 1 ? 'second' : 'seconds';
+
+  String _secondString(int milliseconds){
+    final seconds = milliseconds / 1000;
+    return '$seconds seconds';
+  }
 
   void dispose(){
     timer.cancel();
@@ -35,10 +49,11 @@ class StopWatchState extends State<StopWatch> {
   }
 
   void _startTimer(){
-    timer = Timer.periodic(Duration(seconds: 1), _onTick);
+    timer = Timer.periodic(Duration(milliseconds: 100), _onTick);
 
     setState(() {
       isTicking = true;
+      laps.clear();
     });
   }
 
@@ -54,54 +69,107 @@ class StopWatchState extends State<StopWatch> {
     timer.cancel();
 
     setState(() {
-      seconds = 0;
+      //seconds = 0;
+      milliseconds = 0;
     });
   }
 
-  Widget build (BuildContext context){
+  void _lap(){
+    setState(() {
+      laps.add(milliseconds);
+      _resetTimer();
+    });
+
+    scrollController.animateTo(
+      itemHeight*laps.length,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeIn
+    );
+  }
+  Widget build ( BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Stopwatch'),
+        title: Text(widget.name),
       ),
       body: Column(
+        children: <Widget>[
+          Expanded(child: _buildCounter(context)),
+          Expanded(child: _buildLapDisplay())
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCounter(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child:   Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text(
-            '$seconds ${_secondsString()}',
-            style: Theme.of(context).textTheme.titleMedium,
+            'Lap ${laps.length +1}',
+            style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Colors.white),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                  ),
-                  onPressed: () => _startTimer(),
-                  child: Text('Start')
-              ),
-              SizedBox(width: 20),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                onPressed: () => _stopTimer(),
-                child: Text('Stop'),
-              ),
-              SizedBox(width: 20),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                ),
-                onPressed: () => _resetTimer(),
-                child: Text('Reset'),
-              ),
-            ],
+          Text(
+            _secondString(milliseconds),
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
           ),
+          SizedBox(width: 20),
+          buildControls(),
         ],
+      ),
+    );
+  }
+
+  Widget buildControls() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            ),
+            onPressed: () => _startTimer(),
+            child: Text('Start')
+        ),
+        SizedBox(width:20),
+
+        TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          onPressed: () => _stopTimer(),
+          child: Text('Stop'),
+        ),
+        SizedBox(width:20),
+        TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.yellow),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          onPressed: isTicking ? _lap : null,
+          child: Text('Lap'),
+        )
+      ],
+    );
+  }
+
+  Widget _buildLapDisplay(){
+    return Scrollbar(
+      child: ListView.builder(
+          controller: scrollController,
+          itemExtent: itemHeight,
+          itemCount: laps.length,
+          itemBuilder: (context, index){
+            final milliseconds = laps[index];
+            return ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: 50),
+              title: Text('Lap ${index + 1}'),
+              trailing: Text(_secondString(milliseconds)),
+            );
+          }
       ),
     );
   }
