fad40a418dc6097800b770534cc91f2f562f617b
geoffval <geoffrey.825210040@stu.untar.ac.id>
2023-11-06 20:14:37 +0700

    Read JSON

---
+++ tugas4/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import './pizza.dart';
+
+  void main() {
+    runApp(MyApp());
+  }
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Flutter JSON Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity
+              .adaptivePlatformDensity,
+        ),
+        home: MyHomePage(),
+      );}}
+
+  class MyHomePage extends StatefulWidget {
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
+  }
+  class _MyHomePageState extends State<MyHomePage> {
+
+    String pizzaString = '';
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text('JSON')),
+        body: Container(
+          child: FutureBuilder(
+            future: readJsonFile(),
+            builder: (BuildContext context, AsyncSnapshot<List<Pizza>> pizzas){
+                return ListView.builder(
+                  itemCount: (pizzas.data ==null) ? 0 : pizzas.data?.length,
+                  itemBuilder: (BuildContext context, int position) {
+                    return ListTile(
+                      title: Text(pizzas.data![position].pizzaName),
+                      subtitle: Text(pizzas.data![position].description + ' -€ '
+                          + pizzas.data![position].price.toString()),
+                    );
+                  }
+                );
+            }
+          ),
+        ),
+      );
+    }
+
+    Future<List<Pizza>> readJsonFile() async {
+      String myString = await DefaultAssetBundle.of(context)
+          .loadString('assets/pizzalist.json');
+      List myMap = jsonDecode(myString);
+      List<Pizza> myPizzas = [];
+      myMap.forEach((dynamic pizza) {
+        //Pizza myPizza = Pizza.fromJson(pizza);
+        //myPizzas.add(myPizza);
+        Pizza myPizza = Pizza.fromJsonOrNull(pizza);
+        if (myPizza != null) //Also this is now unnecessary and doesn't work properly because Flutter likes to break compatibility with older versions, thanks google
+          myPizzas.add(myPizza);
+      });
+      String json = convertToJSON(myPizzas);
+      print(json);
+      return myPizzas;
+      /*
+      setState(() {
+        pizzaString = myString;
+      });
+      */
+    }
+
+    @override
+    void initState(){
+      readJsonFile();
+      super.initState();
+    }
+
+    String convertToJSON(List<Pizza> pizzas) {
+      String json = '[';
+      pizzas.forEach((pizza) {
+        json += jsonEncode(pizza);
+      });
+      json += ']';
+      return json;
+    }
+  }
